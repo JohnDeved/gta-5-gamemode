2186eeebd0a33f56f7da86057222e858b952d7fd
Scripticus <Anton.Kurth@gmx.de>
2016-12-31 20:36:05 +0100

    CHA: setWorldData

--- security/clientID.js
+++ security/clientID.js
@@ -1,12 +1,10 @@
-var sessionID = "";
-
 API.onResourceStart.connect(function() {
     API.triggerServerEvent("SESSION_INIT")
 });
 
 API.onServerEventTrigger.connect(function(eventName, args) {
     if (eventName == "SESSION_ID_TRANSFER") {
-        sessionID = args[0]
+        API.setWorldData("sessionID", args[0]);
         API.sendChatMessage("SessionID:" + args[0]);
     }
 });
