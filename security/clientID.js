da062f2dcb8c9348920386a5a015d46bd0be832a
Scripticus <Anton.Kurth@gmx.de>
2016-12-31 20:50:29 +0100

    CHA: Kleiner unnötiger Tweak

--- security/clientID.js
+++ security/clientID.js
@@ -5,6 +5,6 @@ API.onResourceStart.connect(function() {
 API.onServerEventTrigger.connect(function(eventName, args) {
     if (eventName == "SESSION_ID_TRANSFER") {
         sessionID = args[0];
-        API.sendChatMessage("SessionID:" + args[0]);
+        API.sendChatMessage("SessionID: " + args[0]);
     }
 });
