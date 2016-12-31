0c30dc9ad10795ba25c5e2b4aa3d622ffb98cd39
Scripticus <Anton.Kurth@gmx.de>
2016-12-31 21:01:52 +0100

    CHA: Cancel Message

--- rpg/initPlayerLocal.js
+++ rpg/initPlayerLocal.js
@@ -1,8 +1,9 @@
 API.onResourceStart.connect(function() {
     API.triggerServerEvent("ADMIN_VERIFY")
 });
-API.onChatMessage.connect(function(msg) {
+API.onChatMessage.connect(function(msg, e) {
 	if (msg.substr(0,9) == "~!#cid#!:") {
+		e.Cancel = true;
 		API.sendChatMessage("security_session: " + msg.substr(9,msg.length));		
 	}
 });
--- security/clientID.js
+++ security/clientID.js
@@ -9,8 +9,9 @@ API.onServerEventTrigger.connect(function(eventName, args) {
     }
 });
 
-API.onChatMessage.connect(function(msg) {
+API.onChatMessage.connect(function(msg, e) {
 	if (msg.substr(0,9) == "~!#cid#!:") {
+		e.Cancel = true;
 		API.sendChatMessage("security_session: " + msg.substr(9,msg.length));		
 	}
 });
