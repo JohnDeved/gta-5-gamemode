934d3d444ad7d51a5d2befbdb51a9e57487c15e9
Scripticus <Anton.Kurth@gmx.de>
2016-12-31 20:19:38 +0100

    CHA: clientID über SocialClubID

--- security/clientID.cs
+++ security/clientID.cs
@@ -159,7 +159,7 @@ public class clientID : Script
             API.setPlayerClothes(sender, 9, rand.Next(0, 99), 0);
             API.setPlayerClothes(sender, 11, rand.Next(0, 99), 0);
 
-            API.triggerClientEvent(sender, "SESSION_ID_TRANSFER", getCID(API.getPlayerAddress(sender)));
+            API.triggerClientEvent(sender, "SESSION_ID_TRANSFER", getCID(sender.socialClubName));
         }
     }        
 }
