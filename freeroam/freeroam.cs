6b48e1b74921d86ddf90427b56beb77284139647
Scripticus <Anton.Kurth@gmx.de>
2016-12-31 16:52:00 +0100

    FIX: Ob das hilft?

--- freeroam/freeroam.cs
+++ freeroam/freeroam.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
+using System.Globalization;
 using System.Text;
 using GTANetworkServer;
 using GTANetworkShared;
@@ -90,19 +91,19 @@ public class FreeroamScript : Script
         if (name == "SESSION_INIT")
         {
             API.setPlayerSkin(sender, (PedHash)1885233650);
-            _rand = new Random();
+            rand = new Random();
 
-            API.setPlayerClothes(sender, 0, _rand.Next(0, 45), 0);
-            API.setPlayerClothes(sender, 1, _rand.Next(0, 0), 0);
-            API.setPlayerClothes(sender, 2, _rand.Next(0, 36), 4);
-            API.setPlayerClothes(sender, 3, _rand.Next(0, 0), 0);
-            API.setPlayerClothes(sender, 4, _rand.Next(0, 85), 0);
-            API.setPlayerClothes(sender, 5, _rand.Next(0, 69), 0);
-            API.setPlayerClothes(sender, 6, _rand.Next(0, 59), 0);
-            API.setPlayerClothes(sender, 7, _rand.Next(0, 99), 0);
-            API.setPlayerClothes(sender, 8, _rand.Next(0, 99), 0);
-            API.setPlayerClothes(sender, 9, _rand.Next(0, 99), 0);
-            API.setPlayerClothes(sender, 11, _rand.Next(0, 99), 0);
+            API.setPlayerClothes(sender, 0, rand.Next(0, 45), 0);
+            API.setPlayerClothes(sender, 1, rand.Next(0, 0), 0);
+            API.setPlayerClothes(sender, 2, rand.Next(0, 36), 4);
+            API.setPlayerClothes(sender, 3, rand.Next(0, 0), 0);
+            API.setPlayerClothes(sender, 4, rand.Next(0, 85), 0);
+            API.setPlayerClothes(sender, 5, rand.Next(0, 69), 0);
+            API.setPlayerClothes(sender, 6, rand.Next(0, 59), 0);
+            API.setPlayerClothes(sender, 7, rand.Next(0, 99), 0);
+            API.setPlayerClothes(sender, 8, rand.Next(0, 99), 0);
+            API.setPlayerClothes(sender, 9, rand.Next(0, 99), 0);
+            API.setPlayerClothes(sender, 11, rand.Next(0, 99), 0);
         }
     }
 
