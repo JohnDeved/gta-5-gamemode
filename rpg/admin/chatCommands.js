9e35ff84296421a095e2a9b3afb13ecf2f70d3d3
DevSec <arma.devsec@gmail.com>
2016-12-31 22:59:57 +0100

    url

--- rpg/admin/chatCommands.js
+++ rpg/admin/chatCommands.js
@@ -33,7 +33,7 @@ var showDebugInfo = false;
 var adminlevel = "User";
 const CEF = new WebBrowser('html/debug.html');
 const debugCEF_old = new WebBrowser('');
-const modalCEF = new WebBrowser('http://185.62.188.120:3000/webtest/1/2');
+const modalCEF = new WebBrowser('http://185.62.188.120:3000/modal/1/2');
 const testCEF = new WebBrowser('http://185.62.188.120:3000/webtest/Molaron/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-');
 const debugCEF = new WebBrowser('html/debug.html');
 
