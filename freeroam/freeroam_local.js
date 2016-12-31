740c4d15b7e4d3198c5ed916a695c6594d5ca8c3
DevSec <arma.devsec@gmail.com>
2016-12-31 09:26:29 +0100

    debug

--- freeroam/freeroam_local.js
+++ freeroam/freeroam_local.js
@@ -1,43 +1,42 @@
-
 class CefHelper {
-  constructor (resourcePath) {
-    this.path = resourcePath
-    this.open = false
-  }
-
-  show () {
-    if (this.open === false) {
-      this.open = true
-
-      var resolution = API.getScreenResolution()
-
-      this.browser = API.createCefBrowser(resolution.Width, resolution.Height, true)
-      API.waitUntilCefBrowserInit(this.browser)
-      API.setCefBrowserPosition(this.browser, 0, 0)
-      API.loadPageCefBrowser(this.browser, this.path)
-      API.showCursor(true)
+    constructor(resourcePath) {
+        this.path = resourcePath
+        this.open = false
     }
-  }
 
-  destroy () {
-    this.open = false
-    API.destroyCefBrowser(this.browser)
-    API.showCursor(false)
-  }
+    show() {
+        if (this.open === false) {
+            this.open = true
 
-  eval (string) {
-    this.browser.eval(string)
-  }
+            var resolution = API.getScreenResolution()
+
+            this.browser = API.createCefBrowser(resolution.Width, resolution.Height, true)
+            API.waitUntilCefBrowserInit(this.browser)
+            API.setCefBrowserPosition(this.browser, 0, 0)
+            API.loadPageCefBrowser(this.browser, this.path)
+            API.showCursor(true)
+        }
+    }
+
+    destroy() {
+        this.open = false
+        API.destroyCefBrowser(this.browser)
+        API.showCursor(false)
+    }
+
+    eval(string) {
+        this.browser.eval(string)
+    }
 }
 
 var drawSkeletor = false;
 var adminlevel = "User";
-const cef = new CefHelper('html/debug.html');
+const CEF = new CefHelper('html/debug.html');
 const modalCEF = new CefHelper('html/modal.html');
+const debugCEF = new CefHelper('html/debug.html');
 
-API.onUpdate.connect(function (sender, args) {
-    if (drawSkeletor)
-    {
+API.onUpdate.connect(function(sender, args) {
+    if (drawSkeletor) {
         var pont = new Point(0, 1080 - 295)
         var siz = new Size(500, 295)
         API.dxDrawTexture("skeletor.png", pont, siz)
@@ -48,7 +47,7 @@ function ADMIN_executeLocal(shit) {
     eval(shit)
 }
 
-API.onChatCommand.connect(function (msg) {
+API.onChatCommand.connect(function(msg) {
     if (msg == "/spooky") {
         if (drawSkeletor) {
             drawSkeletor = false
@@ -61,55 +60,59 @@ API.onChatCommand.connect(function (msg) {
         modalCEF.show()
     }
 
+    if (msg == "/debugnew") {
+        debugCEF.show()
+    }
+
     if (msg == "/debug") {
         cef.show()
 
-        cef.eval("document.write('\
-<!doctype html>\
-<html>\
-    <head>\
-        <meta charset=\"utf-8\">\
-        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\
-\
-        <link rel=\"stylesheet\" href=\"styles.css\">\
-    </head>\
-    <body>\
-        <div style=\"text-align:center;\">\
-            <textarea id=\"textfield\" name=\"Text1\" cols=\"100\" rows=\"20\"></textarea>\
-            <br/>\
-            <button id=\"buttonClose\">Konsole schließen</button>\
-            <button id=\"executeLocal\">JS Ausführen</button>\
-        </div>\
-    </body>\
-</html>\
-');");
+        cef.eval(`document.write('
+        <!doctype html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1\">
 
-    cef.eval("\
-        document.getElementById('executeLocal').onclick = function () {\
-            try\
-            {\
-                resourceCall('ADMIN_executeLocal',document.getElementById('textfield').value);\
-            }\
-            catch(err) {\
-                document.getElementById('executeLocal').innerHTML = err;\
-            }\
-        };\
-        document.getElementById('buttonClose').onclick = function () {\
-            try\
-            {\
-                resourceCall('cef.destroy');\
-            }\
-            catch(err) {\
+                <link rel="stylesheet" href="styles.css">
+            </head>
+            <body>
+                <div style="text-align:center;">\
+                    <textarea id="textfield" name="Text1" cols="100" rows="20"></textarea>
+                    <br/>
+                    <button id="buttonClose">Konsole schließen</button>
+                    <button id="executeLocal">JS Ausführen</button>
+                </div>
+            </body>
+        </html>
+        ');`);
+
+        cef.eval(`
+        document.getElementById('executeLocal').onclick = function () {
+            try
+            {
+                resourceCall('ADMIN_executeLocal',document.getElementById('textfield').value);
+            }
+            catch(err) {
+                document.getElementById('executeLocal').innerHTML = err;
+            }
+        };
+        document.getElementById('buttonClose').onclick = function () {
+            try
+            {
+                resourceCall('cef.destroy');
+            }
+            catch(err) {
                 document.getElementById('buttonClose').innerHTML = err;\
-            }\
-        };\
-    ");
+            }
+        };
+    `);
     }
     if (msg == "/hide") {
         cef.destroy()
     }
 
-    if(adminlevel == "Admin") {
+    if (adminlevel == "Admin") {
         var match = msg.match(/^\/run/);
 
         if (match != null && match.length > 0) {
@@ -118,13 +121,13 @@ API.onChatCommand.connect(function (msg) {
     }
 });
 
-API.onResourceStart.connect(function () {
+API.onResourceStart.connect(function() {
     API.triggerServerEvent("ADMIN_VERIFY")
     modalCEF.show()
 });
 
-API.onServerEventTrigger.connect(function (eventName, args) {
-    if(eventName == "ADMIN_VERIFY") {
+API.onServerEventTrigger.connect(function(eventName, args) {
+    if (eventName == "ADMIN_VERIFY") {
         adminlevel = args[0]
         API.showShard("Eingeloggt als " + args[0], 2000)
     }
