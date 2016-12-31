3b06b6016f0f2dc509e963c0ab708761dcbe65ef
Scripticus <Anton.Kurth@gmx.de>
2016-12-31 19:48:55 +0100

    ADD: clientID

---
+++ security/clientID.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using GTANetworkServer;
+using GTANetworkShared;
+using System.Threading;
+using MySql.Data.MySqlClient;
+
+
+public class clientID : Script
+{
+    public clientID()
+    {
+        API.onResourceStart += onResourceStart;
+    }
+
+    public Dictionary<string, string> config = new Dictionary<string, string>
+    {
+        {"db_name", "gta_server"},
+        {"db_host", "localhost"},
+        {"db_user", "root"},
+        {"db_table", "security"},
+        {"db_password", "EKbYU6DJNVEwpDTJNFwV3jiG3"},
+        {"cid_length", "25"}
+    }
+
+    static MySqlConnection ConnectToDatabase()
+    {
+        MySqlConnection db_conn = null;
+        try
+        {
+            db_conn = new MySqlConnection(string.Format("server={0};database={1};uid={2};password={3}",config["db_host"],config["db_name"],config["db_user"],config["db_password"]));
+            db_conn.Open();
+        }
+        catch (ArgumentException a_ex)
+        {
+            return null;
+        }
+        catch (MySqlException ex)
+        {
+            return null;
+        }
+        return db_conn;
+    }
+
+    static bool CreateTableIfNotExists()
+    {
+        MySqlConnection db_conn = ConnectToDatabase();
+        if (db_conn == null) return false;
+
+        string query = string.Format(@"CREATE TABLE IF NOT EXISTS `{0}` (
+                                    `socialclub_id` VARCHAR(256) NOT NULL,
+                                    `session_id` VARCHAR({1}) NOT NULL,
+                                    PRIMARY KEY (`socialclub_id`),
+                                    UNIQUE INDEX `socialclub_id_UNIQUE` (`socialclub_id` ASC));", config["db_table"],config["cid_length"]);
+
+        MySqlCommand createTable = new MySqlCommand(query, db_conn);
+        createTable.ExecuteNonQuery();
+        db_conn.Close();
+        return true;
+    }
+
+    static bool DropTableIfNotExists()
+    {
+        MySqlConnection db_conn = ConnectToDatabase();
+        if (db_conn == null) return false;
+
+        string query = string.Format(@"DROP TABLE IF EXISTS `{0}`", config["db_table"]);
+
+        MySqlCommand dropTable = new MySqlCommand(query, db_conn);
+        dropTable.ExecuteNonQuery();
+        db_conn.Close();
+        return true;
+    }
+
+    static string getCID(string socialclub_id)
+    {
+        MySqlConnection db_conn = ConnectToDatabase();
+        if (db_conn == null) return "-7";
+
+        string query = string.Format(@"SELECT session_id FROM `{0}` WHERE socialclub_id='{1}'", config["db_table"], socialclub_id);
+
+        MySqlCommand q_getCID = new MySqlCommand(query, db_conn);
+        StringBuilder session_id = new StringBuilder("");
+        session_id.Append((string)q_getCID.ExecuteScalar());
+
+        if (session_id.ToString() != "")
+        {
+            db_conn.Close();
+            return session_id.ToString();
+        }
+        else
+        {
+            char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789{[]}/()=?+#*~,;.:-_|<>!$%&".ToCharArray();
+            for (int i = 0; i < Convert.ToInt16(config["cid_length"]); i++)
+            {
+                int charindex = (DllEntry._random).Next(chars.Length);
+                session_id.Append(chars[charindex]);
+            }
+
+            query = string.Format(@"INSERT INTO `{0}` SET socialclub_id='{1}', session_id='{2}', adminlevel='{3}'", config["db_table"], socialclub_id, session_id.ToString(), 0);
+            MySqlCommand q_insertCID = new MySqlCommand(query, db_conn);
+            q_insertCID.ExecuteNonQuery();
+
+            db_conn.Close();
+            return session_id.ToString();
+        }
+    }
+
+    static string getSocialClubID(string session_id)
+    {
+        MySqlConnection db_conn = ConnectToDatabase();
+        if (db_conn == null) return "-5";
+
+        string query = string.Format(@"SELECT IFNULL((SELECT socialclub_id FROM `{0}` WHERE session_id='{1}'),NULL)", config["db_table"], session_id);
+
+        MySqlCommand q_getUID = new MySqlCommand(query, db_conn);
+        string socialclub_id;
+        object result = q_getUID.ExecuteScalar();
+
+        if (result != DBNull.Value)
+        {
+            socialclub_id = result.ToString();
+            db_conn.Close();
+            return socialclub_id;
+        }
+        else
+        {
+            return "-2";
+        }
+    }
+
+    private void onResourceStart()
+    {
+        DropTableIfNotExists();
+        CreateTableIfNotExists();
+
+        API.sendChatMessageToAll("~#C2A2DA~", "Result: " + getCID("Molaron"));
+    }
+}
