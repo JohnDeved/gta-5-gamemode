f1808247cad08e76adcf26200f6f4900e8f146d9
Scripticus <Anton.Kurth@gmx.de>
2017-01-02 20:28:45 +0100

    ADD: Erneut die References...

--- rpg/player/func_isPlayerRegistered.cs
+++ rpg/player/func_isPlayerRegistered.cs
@@ -3,8 +3,17 @@ using System.Linq;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Web;
+//GTANetwork
 using GTANetworkServer;
 using GTANetworkShared;
+//External
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json.Linq;
 
 public partial class rpg : Script
 {
