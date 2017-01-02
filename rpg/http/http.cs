9f7c564caba204928dad6fc2e58d8923bbbccfb6
Scripticus <Anton.Kurth@gmx.de>
2017-01-02 01:42:25 +0100

    CHA: Body to Post

--- rpg/http/http.cs
+++ rpg/http/http.cs
@@ -21,144 +21,143 @@ public class http : Script
         API.onResourceStart += onResourceStart;
     }
 
-	public Dictionary<string, int> ClothingParts = new Dictionary<string, int>
-	{
-	    {"face", 0},
-	    {"masc", 1},
-	    {"hair", 2},
-	    {"arms", 3},
-	    {"legs", 4},
-	    {"back", 5},
-	    {"shoes", 6},
-	    {"ties", 7},
-	    {"inner", 8},
-	    {"vest", 9},
-	    {"decals", 10},
-	    {"shirt", 11}
-	};
+    public Dictionary<string, int> ClothingParts = new Dictionary<string, int>
+    {
+        {"face", 0},
+        {"masc", 1},
+        {"hair", 2},
+        {"arms", 3},
+        {"legs", 4},
+        {"back", 5},
+        {"shoes", 6},
+        {"ties", 7},
+        {"inner", 8},
+        {"vest", 9},
+        {"decals", 10},
+        {"shirt", 11}
+    };
 
     private bool VerifyUser(string socialclub_id,string session_id)
     {
-    	foreach (Client player in API.getAllPlayers()) {
-    		if(player.socialClubName == socialclub_id) {
-    			return(API.getEntityData(player, "session_id") == session_id);
-    		}
-    	}
-    	return false;
+        foreach (Client player in API.getAllPlayers()) {
+            if(player.socialClubName == socialclub_id) {
+                return(API.getEntityData(player, "session_id") == session_id);
+            }
+        }
+        return false;
     }
 
     private Client getUser(string socialclub_id)
     {
-    	foreach (Client player in API.getAllPlayers()) {
-    		if(player.socialClubName == socialclub_id) {
-    			return player;
-    		}
-    	}
-    	return null;
+        foreach (Client player in API.getAllPlayers()) {
+            if(player.socialClubName == socialclub_id) {
+                return player;
+            }
+        }
+        return null;
     }
 
     private string VerifyUserWeb(string post_raw) {
-    	API.sendChatMessageToAll("~g~Post",post_raw);
-    	var post = HttpUtility.ParseQueryString(post_raw);
+        API.sendChatMessageToAll("~g~Post",post_raw);
+        var post = HttpUtility.ParseQueryString(post_raw);
 
-    	if(post["socialclub_id"] == "") return "0";
-    	if(post["session_id"] == "") return "0";
+        if(post["socialclub_id"] == "") return "0";
+        if(post["session_id"] == "") return "0";
 
-    	if(VerifyUser(post["socialclub_id"],post["session_id"])) {
-    		return "1";
-    	} else {
-    		return "0";
-    	}
+        if(VerifyUser(post["socialclub_id"],post["session_id"])) {
+            return "1";
+        } else {
+            return "0";
+        }
     }
 
     private void RequestReceived(string args_raw)
     {
-    	var args = HttpUtility.ParseQueryString(args_raw);
-
         API.sendChatMessageToAll("~g~Post:",args_raw);
 
         try
         {
-            var json = JObject.Parse(args_raw);
+            var args = JObject.Parse(args_raw);
+
+            if((string)args.SelectToken("session_id") == "") return;
+            if((string)args.SelectToken("socialclub_id") == "") return;
+            if((string)args.SelectToken("command") == "") return;
+            if((string)args.SelectToken("args") == "") return;
+
+            if(!VerifyUser((string)args.SelectToken("socialclub_id"),(string)args.SelectToken("session_id"))) return;
+
+            Client sender = getUser((string)args.SelectToken("socialclub_id"));
+
+            if(sender == null) return;
+
+            switch((string)args.SelectToken("command")) {
+                case "CEF_CLOSE":
+                    API.triggerClientEvent(sender, "CEF_CLOSE", (string)args.SelectToken("args"));
+                return;
+                case "ADMIN_EVAL":
+                    API.triggerClientEvent(sender, "ADMIN_EVAL", (string)args.SelectToken("args"));
+                return;
+                case "ADMIN_CLOTHES":
+                    string type = (string)args.SelectToken("args.type");
+                    string index = (string)args.SelectToken("args.index");
+
+                    int index_c = int.Parse(index);
+                    int index_s = 0;
+
+                    if((string)args.SelectToken("args.index_s") != "") {
+                        index_s = int.Parse((string)args.SelectToken("args.index_s"));
+                    }
+
+                    API.setPlayerClothes(sender, ClothingParts[type], index_c, index_s);
+                return;
+                case "PLAYER_DISCONNECT":
+                    API.kickPlayer(sender, (string)args.SelectToken("args"));
+                return;
+            }
+
         }catch(Exception e){
-            
+            return;
         }
-
-    	if(args["session_id"] == "") return;
-    	if(args["socialclub_id"] == "") return;
-    	if(args["command"] == "") return;
-    	if(args["args"] == "") return;
-
-    	if(!VerifyUser(args["socialclub_id"],args["session_id"])) return;
-
-    	Client sender = getUser(args["socialclub_id"]);
-
-    	if(sender == null) return;
-
-    	switch(args["command"]) {
-    		case "CEF_CLOSE":
-    			API.triggerClientEvent(sender, "CEF_CLOSE", args["args"]);
-    		return;
-    		case "ADMIN_EVAL":
-    			API.triggerClientEvent(sender, "ADMIN_EVAL", args["args"]);
-    		return;
-    		case "ADMIN_CLOTHES":
-    			string type = args["args"].Split('*')[0];
-    			string index = args["args"].Split('*')[1];
-
-    			int index_c = int.Parse(index);
-    			int index_s = 0;
-
-    			if(args["args"].Split('*').Length > 2) {
-    				index_s = int.Parse(args["args"].Split('*')[2]);
-    			}
-
-    			API.setPlayerClothes(sender, ClothingParts[type], index_c, index_s);
-    		return;
-    		case "PLAYER_DISCONNECT":
-    			API.kickPlayer(sender, args["args"]);
-    		return;
-    	}
     }
 
     private void onResourceStart()
     {
-    	string a = "API.getEntityData(sender, \"session_id\")";
+        string a = "API.getEntityData(sender, \"session_id\")";
         
-		HttpListener listener = new HttpListener();
-		listener.Prefixes.Add("http://185.62.188.120:3001/");
-		listener.Start();
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://185.62.188.120:3001/");
+        listener.Start();
 
-		clientID._random.Next(0,3);
+        clientID._random.Next(0,3);
 
-		while (true)
-		{
-			HttpListenerContext ctx = listener.GetContext();
+        while (true)
+        {
+            HttpListenerContext ctx = listener.GetContext();
 
-			var request = ctx.Request;
-			string post;
-			using (var reader = new StreamReader(request.InputStream,request.ContentEncoding))
-			{
-			    post = reader.ReadToEnd();
-			}
+            var request = ctx.Request;
+            string post;
+            using (var reader = new StreamReader(request.InputStream,request.ContentEncoding))
+            {
+                post = reader.ReadToEnd();
+            }
 
-			string responseText;
+            string responseText;
 
-			if(ctx.Request.Url.ToString() == "http://185.62.188.120:3001/VerifyUser") {
-				responseText = VerifyUserWeb(post);		
-			}else{
-				responseText = "asdfg";
-				RequestReceived(post);	
-			}
+            if(ctx.Request.Url.ToString() == "http://185.62.188.120:3001/VerifyUser") {
+                responseText = VerifyUserWeb(post);     
+            }else{
+                responseText = "asdfg";
+                RequestReceived(post);  
+            }
 
-			byte[] buf = Encoding.UTF8.GetBytes(responseText);
-			ctx.Response.ContentEncoding = Encoding.UTF8;
-			ctx.Response.ContentType = "text/html";
-			ctx.Response.ContentLength64 = buf.Length;		
-			
-			API.sendChatMessageToAll("~g~URL", ctx.Request.Url.ToString());
-			ctx.Response.OutputStream.Write(buf, 0, buf.Length);
-			ctx.Response.Close();
-		}
+            byte[] buf = Encoding.UTF8.GetBytes(responseText);
+            ctx.Response.ContentEncoding = Encoding.UTF8;
+            ctx.Response.ContentType = "text/html";
+            ctx.Response.ContentLength64 = buf.Length;      
+            
+            API.sendChatMessageToAll("~g~URL", ctx.Request.Url.ToString());
+            ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+            ctx.Response.Close();
+        }
     }   
 }
