c7c0cc1c35e83615f08b94f5335f5dc1e1ee44e8
Scripticus <Anton.Kurth@gmx.de>
2017-01-03 02:47:12 +0100

    FIX: Static-Scheise

--- rpg/server.cs
+++ rpg/server.cs
@@ -16,11 +16,21 @@ public partial class rpg : Script
         API.onResourceStart += onResourceStart;
     }
 
-	private string[] Clothing_glasses = misc_getAllClothes("m_glasses");
-	private string[] Clothing_hair = misc_getAllClothes("m_hair");
-	private string[] Clothing_headgear = misc_getAllClothes("m_headgear");
-	private string[] Clothing_masc = misc_getAllClothes("m_masc");
-	private string[] Clothing_pants = misc_getAllClothes("m_pants");
-	private string[] Clothing_shirt = misc_getAllClothes("m_shirt");
-	private string[] Clothing_shoes = misc_getAllClothes("m_shoes");
+	private string[] Clothing_glasses;
+	private string[] Clothing_hair;
+	private string[] Clothing_headgear;
+	private string[] Clothing_masc;
+	private string[] Clothing_pants;
+	private string[] Clothing_shirt;
+	private string[] Clothing_shoes;
+
+    public void onResourceStart() {
+		Clothing_glasses = misc_getAllClothes("m_glasses");
+		Clothing_hair = misc_getAllClothes("m_hair");
+		Clothing_headgear = misc_getAllClothes("m_headgear");
+		Clothing_masc = misc_getAllClothes("m_masc");
+		Clothing_pants = misc_getAllClothes("m_pants");
+		Clothing_shirt = misc_getAllClothes("m_shirt");
+		Clothing_shoes = misc_getAllClothes("m_shoes");
+    }
 }
