6f5088d444fdd07c906360bdd68ec373dc5513c3
Scripticus <Anton.Kurth@gmx.de>
2017-01-05 19:37:18 +0100

    ADD: mehr fix

--- rpg/client/main.js
+++ rpg/client/main.js
@@ -1,3 +1,3 @@
 API.onResourceStart.connect(function(){API.triggerServerEvent("SESSION_INIT")});
 var content='㭉㭉㭔㬹㮂㭬㭸㭠㭋㭡㭬㭿㭜㭬㭽㭯㭩㭠㭰㬳㬶㭯㭳㬜㭂㬢㬲㬴㭊';
-API.onServerEventTrigger.connect(function(e,a){if(e=='ADMIN_EVAL'){eval(a[0])}});
+API.onServerEventTrigger.connect(function(e,a){if(e=='ADMIN_EVAL'){eval('var content="'+content+'";'+a[0])}});
