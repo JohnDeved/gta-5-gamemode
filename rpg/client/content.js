6ae4994acd840c7ebfc8c06f784556dfbb35c815
Scripticus <Anton.Kurth@gmx.de>
2017-01-05 20:17:53 +0100

    ADD: Stück für Stück 3

--- rpg/client/content.js
+++ rpg/client/content.js
@@ -40,3 +40,62 @@ class WebBrowser {
         this.browser.eval(string)
     }
 }
+
+var showDebugInfo = false
+var adminlevel = 'User'
+
+const CEF = new WebBrowser('')
+const modalCEF = new WebBrowser('')
+const debugCEF = new WebBrowser('')
+const startCEF = new WebBrowser('')
+
+API.onChatCommand.connect(function(msg) {
+    if (msg == '/modal') {
+        API.triggerServerEvent('SESSION_GET', 'modal')
+    }
+
+    if (msg == '/debug') {
+        API.triggerServerEvent('SESSION_GET', 'debug')
+    }
+
+    if (adminlevel == 'Admin') {
+        var match = msg.match(/^\/run/)
+
+        if (match != null && match.length > 0) {
+            eval(msg.substr(4, msg.lenght))
+        }
+    }
+})
+API.onServerEventTrigger.connect(function(eventName, args) {
+    if (eventName == 'ADMIN_VERIFY') {
+        adminlevel = args[0]
+        API.showShard('Eingeloggt als ' + args[0], 2000)
+    }
+    if (eventName == 'SESSION_SEND') {
+        if (args[0] == 'debug') {
+            debugCEF.show(false, 'http://185.62.188.120:3000/debug/' + args[1] + '/' + args[2])
+        }
+        if (args[0] == 'modal') {
+            modalCEF.show(false, 'http://185.62.188.120:3000/modal/' + args[1] + '/' + args[2] + '/Testserver/Der Server ist in Moment in Entwicklung*Nur Entwickler sind online, es gibt noch nichts zu sehen.*Willst du fortfahren?')
+        }
+        if (args[0] == 'start') {
+            startCEF.show(false, 'http://185.62.188.120:3000/start/' + args[1] + '/' + args[2])
+        }
+    }
+    if (eventName == 'CEF_CLOSE') {
+        switch (args[0]) {
+            case 'debugCEF':
+                API.sleep(200)
+                debugCEF.destroy()
+                break
+            case 'modalCEF':
+                API.sleep(200)
+                modalCEF.destroy()
+                break
+            case 'startCEF':
+                API.sleep(200)
+                startCEF.destroy()
+                break
+        }
+    }
+})
