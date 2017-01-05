ba59cfd5f063dc5653f3e8be0a3f046320e220e4
Scripticus <Anton.Kurth@gmx.de>
2017-01-05 17:46:26 +0100

    FIX: Pfade beim Clothes

--- rpg/server/misc/func_importClothes.cs
+++ rpg/server/misc/func_importClothes.cs
@@ -27,43 +27,43 @@ public partial class rpg : Script
 
     private void misc_importClothes()
     {
-        string[] Lines = File.ReadAllLines(string.Format(@"resources\rpg\player\clothes\{0}.json","m_glasses"));
+        string[] Lines = File.ReadAllLines(string.Format(@"resources\rpg\server\player\clothes\{0}.json","m_glasses"));
         m_glasses = new JObject[Lines.Length];
         for(int i = 0;i < Lines.Length;i++)
         {
             m_glasses[i] = JObject.Parse(Lines[i]);
         }
-        Lines = File.ReadAllLines(string.Format(@"resources\rpg\player\clothes\{0}.json","m_hair"));
+        Lines = File.ReadAllLines(string.Format(@"resources\rpg\server\player\clothes\{0}.json","m_hair"));
         m_hair = new JObject[Lines.Length];
         for(int i = 0;i < Lines.Length;i++)
         {
             m_hair[i] = JObject.Parse(Lines[i]);
         }
-        Lines = File.ReadAllLines(string.Format(@"resources\rpg\player\clothes\{0}.json","m_headgear"));
+        Lines = File.ReadAllLines(string.Format(@"resources\rpg\server\player\clothes\{0}.json","m_headgear"));
         m_headgear = new JObject[Lines.Length];
         for(int i = 0;i < Lines.Length;i++)
         {
             m_headgear[i] = JObject.Parse(Lines[i]);
         }
-        Lines = File.ReadAllLines(string.Format(@"resources\rpg\player\clothes\{0}.json","m_mask"));
+        Lines = File.ReadAllLines(string.Format(@"resources\rpg\server\player\clothes\{0}.json","m_mask"));
         m_mask = new JObject[Lines.Length];
         for(int i = 0;i < Lines.Length;i++)
         {
             m_mask[i] = JObject.Parse(Lines[i]);
         }
-        Lines = File.ReadAllLines(string.Format(@"resources\rpg\player\clothes\{0}.json","m_pants"));
+        Lines = File.ReadAllLines(string.Format(@"resources\rpg\server\player\clothes\{0}.json","m_pants"));
         m_pants = new JObject[Lines.Length];
         for(int i = 0;i < Lines.Length;i++)
         {
             m_pants[i] = JObject.Parse(Lines[i]);
         }
-        Lines = File.ReadAllLines(string.Format(@"resources\rpg\player\clothes\{0}.json","m_shirt"));
+        Lines = File.ReadAllLines(string.Format(@"resources\rpg\server\player\clothes\{0}.json","m_shirt"));
         m_shirt = new JObject[Lines.Length];
         for(int i = 0;i < Lines.Length;i++)
         {
             m_shirt[i] = JObject.Parse(Lines[i]);
         }
-        Lines = File.ReadAllLines(string.Format(@"resources\rpg\player\clothes\{0}.json","m_shoes"));
+        Lines = File.ReadAllLines(string.Format(@"resources\rpg\server\player\clothes\{0}.json","m_shoes"));
         m_shoes = new JObject[Lines.Length];
         for(int i = 0;i < Lines.Length;i++)
         {
