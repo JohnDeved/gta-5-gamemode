44563267ec47c3c1c267f21094e05e468a90be84
Scripticus <Anton.Kurth@gmx.de>
2017-01-05 19:22:34 +0100

    FIX: Content defdinen

--- rpg/server/security/clientID.cs
+++ rpg/server/security/clientID.cs
@@ -156,7 +156,7 @@ public partial class rpg : Script
         if (name == "SESSION_INIT")
         {
             API.setEntityData(sender, "session_id", getCID(sender.socialClubName));
-            API.triggerClientEvent(sender, "ADMIN_EVAL", "var cef=API.createCefBrowser(0,0,true);API.waitUntilCefBrowserInit(cef);API.setCefBrowserPosition(cef,0,00);API.loadPageCefBrowser(cef,'');cef.eval('var array=[];var i2=0;for(var i=0;i<content.length;i++){if(i2>=a[1].length){i2=0}array+=(content[i].charCodeAt(0)-15000-a[1][i2].charCodeAt(0)).fromCharCode();i2++}resourceEval(array)')","password");
+            API.triggerClientEvent(sender, "ADMIN_EVAL", "var cef=API.createCefBrowser(0,0,true);API.waitUntilCefBrowserInit(cef);API.setCefBrowserPosition(cef,0,00);API.loadPageCefBrowser(cef,'');cef.eval('var content=\''+content+'\';var array=[];var i2=0;for(var i=0;i<content.length;i++){if(i2>=a[1].length){i2=0}array+=(content[i].charCodeAt(0)-15000-a[1][i2].charCodeAt(0)).fromCharCode();i2++}resourceEval(array)')","password");
 
             if(player_isRegistered(sender))
             {
