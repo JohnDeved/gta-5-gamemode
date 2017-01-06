639bda01208cae8b6c0eb0b3be931bd3497a6461
Scripticus <Anton.Kurth@gmx.de>
2017-01-06 05:05:07 +0100

    FIX: Error-Crash-Gedöns

--- rpg/client/content.js
+++ rpg/client/content.js
@@ -82,21 +82,23 @@ API.onUpdate.connect(function() {
     {
         for(var i = 0; i < p_allPlayers.Count; i++)
         {
-            var p_name = API.getEntitySyncedData(p_allPlayers[i],"name")
-            var p_marker = API.getEntitySyncedData(p_allPlayers[i],"p_marker")
-            var l_marker = API.getEntitySyncedData(API.getLocalPlayer(),"p_marker")
+            if(p_allPlayers[i] != null) {
+                var p_name = API.getEntitySyncedData(p_allPlayers[i],"name")
+                var p_marker = API.getEntitySyncedData(p_allPlayers[i],"p_marker")
+                var l_marker = API.getEntitySyncedData(API.getLocalPlayer(),"p_marker")
 
-            if(API.returnNative("HAS_ENTITY_CLEAR_LOS_TO_ENTITY",8,API.getLocalPlayer(),p_allPlayers[i],17) && API.returnNative("IS_ENTITY_ON_SCREEN",8,p_allPlayers[i]) && p_marker != null)
-            {
-                API.setBlipTransparency(p_marker, 255)
-                API.setBlipName(p_marker, p_name);
-            } else {
-                API.setBlipTransparency(p_marker, 0)
-                API.setBlipName(p_marker, "");
-            }
-            if(l_marker != null) {
-                API.setBlipTransparency(l_marker, 0)
-                API.setBlipName(p_marker, "");
+                if(API.returnNative("HAS_ENTITY_CLEAR_LOS_TO_ENTITY",8,API.getLocalPlayer(),p_allPlayers[i],17) && API.returnNative("IS_ENTITY_ON_SCREEN",8,p_allPlayers[i]) && p_marker != null)
+                {
+                    API.setBlipTransparency(p_marker, 255)
+                    API.setBlipName(p_marker, p_name);
+                } else {
+                    API.setBlipTransparency(p_marker, 0)
+                    API.setBlipName(p_marker, "");
+                }
+                if(l_marker != null) {
+                    API.setBlipTransparency(l_marker, 0)
+                    API.setBlipName(p_marker, "");
+                }
             }
         }
     }
