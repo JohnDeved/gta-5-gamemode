64eab84f99416c51c86a22f8603e64fd584dfef4
Scripticus <Anton.Kurth@gmx.de>
2017-01-06 03:00:04 +0100

    CHA: Class verschoben

---
+++ rpg/client/class_cef.js
@@ -0,0 +1,36 @@
+class WebBrowser {
+    constructor(resourcePath) {
+        this.path = resourcePath
+        this.open = false
+    }
+
+    show(global, opt_path) {
+        if (this.open === false) {
+            this.open = true
+
+            var resolution = API.getScreenResolution()
+
+            this.browser = API.createCefBrowser(resolution.Width, resolution.Height, global)
+            API.waitUntilCefBrowserInit(this.browser)
+            API.setCefBrowserPosition(this.browser, 0, 0)
+
+            if (opt_path) {
+                API.loadPageCefBrowser(this.browser, opt_path)
+            } else {
+                API.loadPageCefBrowser(this.browser, this.path)
+            }
+
+            API.showCursor(true)
+        }
+    }
+
+    destroy() {
+        this.open = false
+        API.destroyCefBrowser(this.browser)
+        API.showCursor(false)
+    }
+
+    eval(string) {
+        this.browser.eval(string)
+    }
+}
--- rpg/client/content.js
+++ rpg/client/content.js
@@ -4,42 +4,6 @@ API.onServerEventTrigger.connect(function(eventName, args) {
         API.sendChatMessage('SessionID: ' + args[0])
     }
 })
-class WebBrowser {
-    constructor(resourcePath) {
-        this.path = resourcePath
-        this.open = false
-    }
-
-    show(global, opt_path) {
-        if (this.open === false) {
-            this.open = true
-
-            var resolution = API.getScreenResolution()
-
-            this.browser = API.createCefBrowser(resolution.Width, resolution.Height, global)
-            API.waitUntilCefBrowserInit(this.browser)
-            API.setCefBrowserPosition(this.browser, 0, 0)
-
-            if (opt_path) {
-                API.loadPageCefBrowser(this.browser, opt_path)
-            } else {
-                API.loadPageCefBrowser(this.browser, this.path)
-            }
-
-            API.showCursor(true)
-        }
-    }
-
-    destroy() {
-        this.open = false
-        API.destroyCefBrowser(this.browser)
-        API.showCursor(false)
-    }
-
-    eval(string) {
-        this.browser.eval(string)
-    }
-}
 
 var showDebugInfo = false
 var adminlevel = 'User'
@@ -58,7 +22,7 @@ API.onChatCommand.connect(function(msg) {
         API.triggerServerEvent('SESSION_GET', 'debug')
     }
 
-    if (msg == '/cloth') {
+    if (msg == '/cloth') {//
         API.triggerServerEvent('SESSION_GET', 'clothing')
     }    
 
