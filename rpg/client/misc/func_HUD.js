b94a69cf1d8a49c543e8e671fe8c35c7dfb43ed7
Scripticus <Anton.Kurth@gmx.de>
2017-01-08 22:20:51 +0100

    FIX: Alle Spieler berücksichtigen

--- rpg/client/misc/func_HUD.js
+++ rpg/client/misc/func_HUD.js
@@ -1,25 +1,33 @@
 API.onUpdate.connect(function() {
 	var money = API.getEntitySyncedData(API.getLocalPlayer(), "money");
 
-	var player = API.getLocalPlayer();
-	var head_pos = API.returnNative("GET_PED_BONE_COORDS",5,player,12844,0);
-	skel_head = API.worldToScreen(new Vector3(head_pos.X,head_pos.Y,head_pos.Z + .3));
+	var p_allPlayers = API.getWorldSyncedData('p_allPlayers')
 
-	if(API.isPlayerDead(player)) {
-		API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 75, 75, 75, 255, 4, 1, false, true, 256);
-	} else {
+    if (p_allPlayers != null) {
+        for (var i = 0; i < p_allPlayers.Count; i++) {
+            if (p_allPlayers[i] != null) {
+				var player = p_allPlayers[i];
+				var head_pos = API.returnNative("GET_PED_BONE_COORDS",5,player,12844,0);
+				skel_head = API.worldToScreen(new Vector3(head_pos.X,head_pos.Y,head_pos.Z + .3));
 
-		switch(API.getEntitySyncedData(player,"role"))
-		{
-			case"Civilian":
-				API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 255, 255, 255, 255, 4, 1, false, true, 256);
-				break;
-			case"Police":
-				API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 0, 76, 152, 255, 4, 1, false, true, 256);
-				break;
-			case"Medic":
-				API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 255, 187, 0, 255, 4, 1, false, true, 256);
-				break;
+				if(API.isPlayerDead(player)) {
+					API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 75, 75, 75, 255, 4, 1, false, true, 256);
+				} else {
+
+					switch(API.getEntitySyncedData(player,"role"))
+					{
+						case"Civilian":
+							API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 255, 255, 255, 255, 4, 1, false, true, 256);
+							break;
+						case"Police":
+							API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 0, 76, 152, 255, 4, 1, false, true, 256);
+							break;
+						case"Medic":
+							API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 255, 187, 0, 255, 4, 1, false, true, 256);
+							break;
+					}
+				}
+			}
 		}
 	}	
 })
