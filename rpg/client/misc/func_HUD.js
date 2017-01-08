d8a1c50c3bb5bf3854c82bbbd8ae952159383df5
Scripticus <Anton.Kurth@gmx.de>
2017-01-08 22:39:24 +0100

    add: dist-check

--- rpg/client/misc/func_HUD.js
+++ rpg/client/misc/func_HUD.js
@@ -10,21 +10,23 @@ API.onUpdate.connect(function() {
 				var head_pos = API.returnNative("GET_PED_BONE_COORDS",5,player,12844,0);
 				skel_head = API.worldToScreen(new Vector3(head_pos.X,head_pos.Y,head_pos.Z + .3));
 
-				if(API.isPlayerDead(player)) {
-					API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 75, 75, 75, 255, 4, 1, false, true, 256);
-				} else {
+				if(API.returnNative('HAS_ENTITY_CLEAR_LOS_TO_ENTITY', 8, API.getLocalPlayer(), player, 17) && API.getEntityPosition(API.getLocalPlayer()).Substract(API.getEntityPosition(player))) {
+					if(API.isPlayerDead(player)) {
+						API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 75, 75, 75, 255, 4, 1, false, true, 256);
+					} else {
 
-					switch(API.getEntitySyncedData(player,"role"))
-					{
-						case"Civilian":
-							API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 255, 255, 255, 255, 4, 1, false, true, 256);
-							break;
-						case"Police":
-							API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 0, 76, 152, 255, 4, 1, false, true, 256);
-							break;
-						case"Medic":
-							API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 255, 187, 0, 255, 4, 1, false, true, 256);
-							break;
+						switch(API.getEntitySyncedData(player,"role"))
+						{
+							case"Civilian":
+								API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 255, 255, 255, 255, 4, 1, false, true, 256);
+								break;
+							case"Police":
+								API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 0, 76, 152, 255, 4, 1, false, true, 256);
+								break;
+							case"Medic":
+								API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 255, 187, 0, 255, 4, 1, false, true, 256);
+								break;
+						}
 					}
 				}
 			}
