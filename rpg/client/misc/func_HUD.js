527192dabf9c33509b00160d1cc04c333bfe190b
Scripticus <Anton.Kurth@gmx.de>
2017-01-09 12:26:02 +0100

    CHA: Doch so

--- rpg/client/misc/func_HUD.js
+++ rpg/client/misc/func_HUD.js
@@ -7,14 +7,14 @@ API.onUpdate.connect(function() {
         for (var i = 0; i < p_allPlayers.Count; i++) {
             if (p_allPlayers[i] != null) {
 				var player = p_allPlayers[i];
-				var head_pos = API.getEntityPosition(player);
+				var head_pos = API.returnNative("GET_PED_BONE_COORDS",5,player,12844,0);
 				var local_pos = API.getEntityPosition(API.getLocalPlayer());
 
 				var distance = Math.sqrt(Math.pow(head_pos.X - local_pos.X,2) + Math.pow(head_pos.Y - local_pos.Y,2) + Math.pow(head_pos.Z - local_pos.Z,2));
 
-				skel_head = API.worldToScreen(new Vector3(head_pos.X,head_pos.Y,head_pos.Z + 1));
+				skel_head = API.worldToScreen(new Vector3(head_pos.X,head_pos.Y,head_pos.Z + .3));
 
-				if(API.returnNative('HAS_ENTITY_CLEAR_LOS_TO_ENTITY', 8, API.getLocalPlayer(), player, 17) && distance < 15) {
+				if(!API.isPlayerInAnyVehicle(player) && API.returnNative('HAS_ENTITY_CLEAR_LOS_TO_ENTITY', 8, API.getLocalPlayer(), player, 17) && distance < 15) {
 					if(API.isPlayerDead(player)) {
 						API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 75, 75, 75, 255, 4, 1, false, true, 256);
 					} else {
