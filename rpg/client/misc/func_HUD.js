b97b307bc3f40b05dae2ee78bc6b1b012104aca9
DevSec <arma.devsec@gmail.com>
2017-01-10 19:41:33 +0100

    -

--- rpg/client/misc/func_HUD.js
+++ rpg/client/misc/func_HUD.js
@@ -14,26 +14,24 @@ function hudUpdate() {
 
                 skel_head = API.worldToScreen(new Vector3(head_pos.X, head_pos.Y, head_pos.Z + 0.3))
 
-				if(!API.isPlayerInAnyVehicle(player) && API.returnNative('HAS_ENTITY_CLEAR_LOS_TO_ENTITY', 8, API.getLocalPlayer(), player, 17) && distance < 15) {
-					if(API.isPlayerDead(player)) {
-						API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 75, 75, 75, 255, 4, 1, false, true, 256);
-					} else {
-
-						switch(API.getEntitySyncedData(player,"role"))
-						{
-							case"Civilian":
-								API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 181, 0, 0, 255, 4, 1, false, true, 256);
-								break;
-							case"Police":
-								API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 0, 76, 152, 255, 4, 1, false, true, 256);
-								break;
-							case"Medic":
-								API.drawText(API.getEntitySyncedData(player,"name"), skel_head.X, skel_head.Y, .3, 255, 187, 0, 255, 4, 1, false, true, 256);
-								break;
-						}
-					}
-				}
-			}
-		}
-	}	
+                if (!API.isPlayerInAnyVehicle(player) && API.returnNative('HAS_ENTITY_CLEAR_LOS_TO_ENTITY', 8, API.getLocalPlayer(), player, 17) && distance < 15) {
+                    if (API.isPlayerDead(player)) {
+                        API.drawText(API.getEntitySyncedData(player, 'name'), skel_head.X, skel_head.Y, 0.3, 75, 75, 75, 255, 4, 1, false, true, 256)
+                    } else {
+                        switch (API.getEntitySyncedData(player, 'role')) {
+                            case 'Civilian':
+                                API.drawText(API.getEntitySyncedData(player, 'name'), skel_head.X, skel_head.Y, 0.3, 181, 0, 0, 255, 4, 1, false, true, 256)
+                                break
+                            case 'Police':
+                                API.drawText(API.getEntitySyncedData(player, 'name'), skel_head.X, skel_head.Y, 0.3, 0, 76, 152, 255, 4, 1, false, true, 256)
+                                break
+                            case 'Medic':
+                                API.drawText(API.getEntitySyncedData(player, 'name'), skel_head.X, skel_head.Y, 0.3, 255, 187, 0, 255, 4, 1, false, true, 256)
+                                break
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
