ac193854f89d8aaace751af08e2b2e114da47cd4
DevSec <arma.devsec@gmail.com>
2017-01-21 22:08:59 +0100

    CHA: ExecuteReader

--- rpg/server/server.cs
+++ rpg/server/server.cs
@@ -144,16 +144,11 @@ public partial class rpg : Script
             if (db_conn == null) return;
 
             string query = "SELECT * FROM vehicleshop WHERE tier=1";
-            var reader = new MySqlCommand(query, db_conn).ExecuteNonQuery();
-
-            do {
-                int count = reader.FieldCount;
-                while(reader.Read()) {
-                    for(int i = 0 ; i < count ; i++) {
-                        Console.WriteLine(reader.GetValue(i));
-                    }
-                }
-            } while (reader.NextResult())
+            var reader = new MySqlCommand(query, db_conn).ExecuteReader();
+            while(reader.Read())
+            {
+                break;
+            }
         /*</getCars>*/
         API.sendChatMessageToPlayer(sender, "~o~SERVER: ~c~Fahrzeuge wurden Erstellt!");
     }
