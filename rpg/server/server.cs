7042a13ee346f9761779a87500fca5c310746552
DevSec <arma.devsec@gmail.com>
2017-01-21 21:20:18 +0100

    dsadsa

--- rpg/server/server.cs
+++ rpg/server/server.cs
@@ -144,11 +144,11 @@ public partial class rpg : Script
             if (db_conn == null) return;
 
             string query = "SELECT * FROM vehicleshop WHERE tier=1";
-            var reader = new MySqlCommand(query, db_conn).ExecuteNonQuery();
-            while(reader.Read())
-            {
-                var veh = API.createVehicle(new VehicleHash "-1216765807", new Vector3((int)reader["x"], (int)reader["y"], (int)reader["z"]), new Vector3(0, 0, (int)reader["rotation"]), 0, 0);
-            }
+            new MySqlCommand(query, db_conn).ExecuteNonQuery();
+            // while(reader.Read())
+            // {
+            //     var veh = API.createVehicle(new VehicleHash "-1216765807", new Vector3((int)reader["x"], (int)reader["y"], (int)reader["z"]), new Vector3(0, 0, (int)reader["rotation"]), 0, 0);
+            // }
         /*</getCar>*/
         API.sendChatMessageToPlayer(sender, "~o~SERVER: ~c~Gespawnt...");
     }
