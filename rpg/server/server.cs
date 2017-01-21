08a3b47424360bb04e7f8da3070a623484b9d677
DevSec <arma.devsec@gmail.com>
2017-01-21 21:55:09 +0100

    CHA: stackoverflow

--- rpg/server/server.cs
+++ rpg/server/server.cs
@@ -145,6 +145,15 @@ public partial class rpg : Script
 
             string query = "SELECT * FROM vehicleshop WHERE tier=1";
             var reader = new MySqlCommand(query, db_conn).ExecuteNonQuery();
+
+            do {
+                int count = reader.FieldCount;
+                while(reader.Read()) {
+                    for(int i = 0 ; i < count ; i++) {
+                        Console.WriteLine(reader.GetValue(i));
+                    }
+                }
+            } while (reader.NextResult())
         /*</getCars>*/
         API.sendChatMessageToPlayer(sender, "~o~SERVER: ~c~Fahrzeuge wurden Erstellt!");
     }
