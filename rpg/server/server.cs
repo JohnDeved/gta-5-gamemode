6cb7b9f522902c32076aaee2c894a6f6a226b69f
DevSec <arma.devsec@gmail.com>
2017-01-22 03:13:38 +0100

    ADD: kauf command

--- rpg/server/server.cs
+++ rpg/server/server.cs
@@ -203,4 +203,20 @@ public partial class rpg : Script
             API.sendChatMessageToPlayer(sender, "~r~ERR: ~c~Du bist in keinen Fahrzeug!");
         }
     }
+
+    [Command("vbuy")]
+    public void buyCarCommand(Client sender)
+    {
+        if (API.isPlayerInAnyVehicle(sender)) {
+            var veh = API.getPlayerVehicle(sender);
+            if(API.getVehicleNumberPlate(veh) == "BUY NOW") {
+                API.setEntityPositionFrozen(veh, false);
+                API.sendChatMessageToPlayer(player, "~o~SERVER: ~c~Fahrzeug erfolgreich gekauft!");
+            } else {
+                API.sendChatMessageToPlayer(sender, "~r~ERR: ~c~Du kannst dieses Fahrzeug nicht kaufen!");
+            }
+        } else {
+            API.sendChatMessageToPlayer(sender, "~r~ERR: ~c~Du bist in keinen Fahrzeug!");
+        }
+    }
 }
