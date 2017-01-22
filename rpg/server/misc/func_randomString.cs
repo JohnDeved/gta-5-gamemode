7fa12f7cd1abffdcb82f8ba092f7a5844106b152
DevSec <arma.devsec@gmail.com>
2017-01-22 16:35:32 +0100

    iwas stimmt da wieder nicht :b

--- rpg/server/misc/func_randomString.cs
+++ rpg/server/misc/func_randomString.cs
@@ -17,16 +17,7 @@ using Newtonsoft.Json.Linq;
 
 public partial class rpg : Script
 {
-    private bool misc_randomString(int size, string chars)
+    private bool misc_randomString()
     {
-        var stringChars = new char[size];
-        var random = new Random();
-
-        for (int i = 0; i < stringChars.Length; i++)
-        {
-            stringChars[i] = chars[random.Next(chars.Length)];
-        }
-
-        return new String(stringChars);
     }
 }
