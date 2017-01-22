c8b28a03ba5dacf8c6a7c0fc25a52aaf4d0b5466
DevSec <arma.devsec@gmail.com>
2017-01-22 16:33:18 +0100

    ADD: random string fnc

---
+++ rpg/server/misc/func_randomString.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Web;
+//GTANetwork
+using GTANetworkServer;
+using GTANetworkShared;
+//External
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json.Linq;
+
+public partial class rpg : Script
+{
+    private bool misc_randomString(int size, string chars)
+    {
+        var stringChars = new char[size];
+        var random = new Random();
+
+        for (int i = 0; i < stringChars.Length; i++)
+        {
+            stringChars[i] = chars[random.Next(chars.Length)];
+        }
+
+        return new String(stringChars);
+    }
+}
